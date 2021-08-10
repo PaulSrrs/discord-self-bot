0578e0c58cb9bcb8b754947d2c0b51a9ec089620
Paul Surrans <44446520+PaulSrrs@users.noreply.github.com>
2021-08-10 22:59:08 +0200

    feat(discord_self_bot): add readme, license and discord_self_bot

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # discord-self-bot
-This simple Python discord bot is useful to send the same message all X milliseconds as a user.
+
+Dependencies:
+    Python3
+
+This simple Python discord script (also called self-bot) is useful to send the same message all X seconds to a specific channel.
+The message will be send as a user, not a bot.
+You will need your private discord user token and the channel id. It's up to you to find it.
+
+The user-agent used is Mozilla/5.0, it can be changed.
+
+This script is functionnal with Discord v8 api.
+
+Don't forget to add the right to execute this file with chmod command :
+
+```chmod +x discord_self_bot.py```
+
+USAGE:
+
+```discord_self_bot.py [-h] [-ua USER_AGENT] -t TOKEN -m MESSAGE -s SECONDS -c CHANNEL
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -ua USER_AGENT, --user-agent USER_AGENT
+                        User agent used by the program, default value : Mozilla/5.0
+  -t TOKEN, --token TOKEN
+                        Your discord user token, it's up to you to find it !
+  -m MESSAGE, --message MESSAGE
+                        The message you want to post on the channel
+  -s SECONDS, --seconds SECONDS
+                        Your discord unique token, it's up to you to find it (google is your friend) !
+  -c CHANNEL, --channel CHANNEL```
+
+I, Paul Surrans, am not responsable of any consequences using this script.
+
+Made with love by Paul Surrans, Student at Epitech Lille, France.
+
+License: MIT
+
+*All credits goes to Kim Cho, c.f :
+[Quora topic](https://www.quora.com/I-want-to-automatically-post-a-message-every-24-hours-on-my-Discord-server-using-my-own-account-not-a-bot-account-Is-this-possible-and-if-so-how)*
+
+
+
---
+++ discord_self_bot.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+from http.client import HTTPSConnection 
+from sys import stderr 
+from json import dumps 
+from time import sleep
+from datetime import datetime
+import sys
+import argparse
+
+# FUNCTION TO DEAL WITH NEGATIVE SECONDS INPUT
+def check_positive(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError("%s : seconds cannot be negative !" % value)
+    return ivalue
+
+def send_message(args):
+	connection = HTTPSConnection("discordapp.com", 443)
+	header_data = { 
+		"content-type": "application/json", 
+		"user-agent": args.user_agent, 
+		"authorization": args.token, 
+		"host": "discordapp.com"
+	}
+	message_data = { 
+		"content": args.message, 
+		"tts": "false",
+	}
+	try: 
+		connection.request("POST", f"/api/v8/channels/{args.channel}/messages", dumps(message_data), header_data) 
+		resp = connection.getresponse() 
+		if 199 < resp.status < 300: 
+			print(f"Message '{args.message} sent at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}") 
+			pass 
+		else: 
+			stderr.write(f"Received HTTP {resp.status}: {resp.reason}\n") 
+			pass
+	except: 
+		stderr.write("Failed to send_message\n") 	
+ 
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-ua", "--user-agent", type=str, default="Mozilla/5.0", help="User agent used by the program, default value : Mozilla/5.0")
+	parser.add_argument("-t", "--token", type=str, help="Your discord user token, it's up to you to find it !", required=True)
+	parser.add_argument("-m", "--message", type=str, help="The message you want to post on the channel", required=True)
+	parser.add_argument("-s", "--seconds", type=check_positive, help="Your discord unique token, it's up to you to find it (google is your friend) !", required=True)
+	parser.add_argument("-c", "--channel", type=str, help="Id of the discord channel, again, it's up to you to find it (google is your friend) !", required=True)
+	try:
+		args = parser.parse_args()
+	except:
+		sys.exit(1)
+	while True:     			# Infinite loop 
+		send_message(args)      # Send the message 
+		sleep(args.seconds) 	# Wait X seconds to repeat
